e2f061f201ec4d56302df0e268a8b76a5c084760
Ty HA <bichty.ha@gmail.com>
2024-05-04 10:50:34 +0200

    Update QR code, Tailwind, Flowbite, Web3Auth

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,9 +2,15 @@ import type { Config } from "tailwindcss";
 const flowbite = require("flowbite-react/tailwind");
 const config: Config = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./page.tsx",
+    "./index.tsx",
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./node_modules/flowbite-react/lib/**/*.js",
+    "./public/**/*.html",
+    "node_modules/flowbite-react/lib/esm/**/*.js",
+    flowbite.content(),
   ],
   theme: {
     extend: {
