122f005a5ef909e4d0fad3d0ca54bdc8b44a0f89
Ty-HA <bichty.ha@gmail.com>
2024-06-26 16:21:09 +0200

    build test

--- src/app/Register/page.tsx
+++ src/app/Register/page.tsx
@@ -1,1 +1,9 @@
-// Register New Veterinarian to create his DiD
+// Register New Veterinarian to create his DiD// src/app/Register/page.tsx
+
+import React from 'react';
+
+const Register = () => {
+  return <div>Register New Veterinarian</div>;
+};
+
+export default Register;
